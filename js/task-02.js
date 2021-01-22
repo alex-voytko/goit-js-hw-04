cd0d60c68e585fbc2a2d451184e181acb6ade1d4
alex-voytko <voytko1994@gmail.com>
2021-01-22 15:13:37 +0200

    1.3 final

--- js/task-02.js
+++ js/task-02.js
@@ -6,27 +6,28 @@ const inventory = {
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
-
     this.items = this.items.filter(item => item !== itemName);
   },
 };
-
 const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking action on ${itemName}`);
   action(itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+const inventoryAddAct = inventory.add.bind(inventory)
+const inventoryRemoveAct = inventory.remove.bind(inventory)
+
+invokeInventoryAction('Medkit', inventoryAddAct);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
+invokeInventoryAction('Gas mask', inventoryRemoveAct);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
 
 console.log(inventory.items); // ['Knife', 'Medkit']
 
 
-  // Ура, все работает :)
+// Ура, все работает :)
